02762f0be2014c482ba8b908ba4d0c520c319e33
AlcidesFabianFernandezArambulo <affa080290@gmail.com>
2026-02-13 23:02:09 -0300

    Configuracion de base de datos MongoBD

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const dotenv = require("dotenv").config()
+require("dotenv").config()
 const express = require('express')
+const mongoose = require("mongoose")
+
 const app = express()
 const PORT = process.env.PORT //Trae el valor de PORT de el archico .env
 
@@ -7,6 +9,18 @@ app.get('/', (req, res) =>{
     res.send('hello World!')
 })
 
-app.listen(PORT, ()=>{
-    console.log(`App excuchando en puerto ${PORT}`)
-})
+/*conexion a la base de datos*/
+mongoose.connect(process.env.MONGO_DB_URL, {
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        }).then(()=>{
+            console.log("Conexion a MongoDB exitosa")
+            app.listen(PORT, () =>{
+                console.log(`App escuchando en puerto ${PORT}`)
+            })
+        })
+        .catch(err => {
+            console.log("Error al conectar a MongoDB", err)
+        })
+
+
